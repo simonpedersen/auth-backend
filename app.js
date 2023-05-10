f1b68fd0714d9055e1559b891c5cc0e0d574ad40
Simon pedersen <61418564+simonpedersen@users.noreply.github.com>
2023-05-10 10:16:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -158,21 +158,21 @@ app.put("/update-user-info", (request, response) => {
       ) {
         user.age = parseInt(request.body.personalInfoObject.age);
       }
-      // if (
-      //   request.body.experienceObject &&
-      //   request.body.experienceObject.languages
-      // ) {
-      //   user.languages = request.bodyrequest.body.experienceObject.languages;
-      // }
+      if (
+        request.body.experienceObject &&
+        request.body.experienceObject.languages
+      ) {
+        user.languages = request.body.experienceObject.languages;
+      }
       if (request.body.files) {
         user.files = request.body.files;
       }
-      // if (
-      //   request.body.personalInfoObject &&
-      //   request.body.personalInfoObject.phoneNumber
-      // ) {
-      //   user.phone_number = request.body.personalInfoObject.phoneNumber;
-      // }
+      if (
+        request.body.personalInfoObject &&
+        request.body.personalInfoObject.phoneNumber
+      ) {
+        user.phone_number = request.body.personalInfoObject.phoneNumber;
+      }
       if (
         request.body.personalInfoObject &&
         request.body.personalInfoObject.address
